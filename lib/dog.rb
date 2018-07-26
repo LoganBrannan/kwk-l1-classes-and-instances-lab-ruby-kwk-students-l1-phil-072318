45364b4c1b2996b6e98318cb6c3e8203482efbc6
LoganBrannan <loganbrannan13@gmail.com>
2018-07-26 18:49:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # dog.rb
+
+    class Dog #start of my new object
+    def initialize(name, age, breed) #this is called whenever you create a new instance of a dog
+        @name = name
+        @age = age
+        @breed = breed
+    end
+    
+    def name
+        return @name
+    end
+    
+    def age
+        return @age
+    end
+      def breed
+      return @breed
+      end
+      dog = Dog.new("fido")
+  dog2 = Dog.new("snoopy")
+  dog3 = Dog.new("lassie")
+end
+ dog = Dog.new("fido")
+ dog2 = Dog.new("snoopy")
+ dog3 = Dog.new("lassie")
+  
